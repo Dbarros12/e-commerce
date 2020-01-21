029c3e8ff44793e049b54792a847b8d11497ecd4
Dbarros12 <barros2506@gmail.com>
2020-01-21 09:39:00 -0300

    Recharges

---
+++ src/app/pages/recharges/recharges.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    //moduleId: module.id,
+    selector: 'recharges',
+    templateUrl: 'recharges.component.html',
+    styleUrls: ['recharges.component.scss']
+})
+export class RechargesComponent {
+
+}
